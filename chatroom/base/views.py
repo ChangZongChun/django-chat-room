51fcabc5f70674ce3abf70615a55f42fc927b924
Ubuntu <ubuntu@ip-172-31-15-118.ap-northeast-1.compute.internal>
2023-03-07 13:51:15 +0000

    email type

--- chatroom/base/views.py
+++ chatroom/base/views.py
@@ -24,7 +24,7 @@ def loginPage(request):
         return redirect('home')
 
     if request.method == 'POST':
-        email = request.POST.get('email').lower()
+        email = request.POST.get('email')
         password = request.POST.get('password')
 
         try :
