70e580881a3074bb8880e36591294caf405241a6
ChangZongChun <debbychang0121@gmail.com>
2023-03-16 21:04:53 +0800

    modify login

--- chatroom/base/models.py
+++ chatroom/base/models.py
@@ -6,12 +6,12 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    name = models.CharField(unique=True, max_length=200, null=True)
-    email = models.EmailField(null=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
     avatar = models.ImageField(null=True, upload_to="profile_pics/", default="avatar.svg")
 
-    # USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
 
