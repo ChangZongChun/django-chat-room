e54ddcdcfcd3f3abcc94789ec161ae7d7b486e3b
ChangZongChun <debbychang0121@gmail.com>
2023-08-09 19:42:17 +0800

    without superuser

--- chatroom/entrypoint.sh
+++ chatroom/entrypoint.sh
@@ -6,8 +6,8 @@ echo 'Run migration'
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-echo 'Create Super User'
-python3 manage.py createsuperuser --noinput --email $SUPERUSER_EMAIL || echo "Super user already created"
+# echo 'Create Super User'
+# python3 manage.py createsuperuser --noinput --email $SUPERUSER_EMAIL || echo "Super user already created"
 
 echo 'Collect Static'
 python3 manage.py collectstatic --noinput
